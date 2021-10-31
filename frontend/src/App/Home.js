ddc996c7d40043f2fb71086c00a5edea09ebbb92
unknown <stevenchristpa2001@gmail.com>
2021-10-31 14:46:46 +0700

    Penambahan homepage

--- frontend/src/App/Home.js
+++ frontend/src/App/Home.js
@@ -4,16 +4,46 @@ import './Home.css';
 function Home(){
 	return(
 		<>
-			<div className = "par1"></div>
+			<div className = "par1">
+				<div className="title mt-3">
+					<h2 className="text-white text-center">Coba App</h2>
+				</div>
+				<div className="navigation">
+					<ul className="navbar-nav d-flex flex-row m-auto w-75 justify-content-between">
+						<li><a className="nav-link text-white" href="#">Home</a></li>
+						<li><a className="nav-link text-white" href="#">About Us</a></li>
+						<li><a className="nav-link text-white" href="#">Our Products</a></li>
+						<li><a className="nav-link text-white" href="#">Services</a></li>
+						<li><a className="nav-link text-white" href="#">Account</a></li>
+					</ul>
+				</div>
+			</div>
 
-			<div className="container">
-				<div className="d-flex flex-column">
-					<div className="flex-row">
-						<h1>Section 1</h1>
+			<div className="container mt-4">
+				<div>
+					<h1 className="text-white text-center">Feature</h1>
+				</div>
+				<div className="d-flex flex-row mb-5 mt-5">
+					<div className="card me-2">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
+						<div>
+							<h2>Event</h2>
+						</div>
 					</div>
 
-					<div>
-						<h1>Section 2</h1>
+					<div className="card me-2">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
+						<div>
+							<h2>Event</h2>
+						</div>
+					</div>
+
+					<div className="card me-2">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
+						<div>
+							<h2>Event</h2>
+						</div>
+						
 					</div>
 				</div>
 			</div>
@@ -21,33 +51,50 @@ function Home(){
 			<div className = "par2"></div>
 
 			<div className="container">
-				<div className="d-flex flex-column">
-					<div className="flex-row">
-						<h1>Section 1</h1>
+				<div className="d-flex flex-row mb-5 mt-5">
+					<div className="col me-5">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
 					</div>
-
-					<div>
-						<h1>Section 2</h1>
+					
+					<div className="col">
+						<h2 className="text-white">Title</h2>
+						<h5 className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque sed mauris eget posuere. Quisque malesuada nibh ac massa imperdiet, porttitor vulputate justo volutpat. Etiam dolor risus, interdum a massa sed, faucibus finibus sem. Nulla molestie molestie quam, a consequat neque molestie sit amet. Vivamus efficitur faucibus risus quis convallis. Morbi id ex volutpat, pretium nulla at, luctus justo. Proin condimentum finibus lorem et tristique.</h5>
 					</div>
 				</div>
 			</div>
+
+			<div className = "par2"></div>
 
 			<div className="container">
-				<div className="d-flex flex-column">
-					<div className="flex-row">
-						<h1>Section 1</h1>
+				<div className="d-flex flex-row-reverse mb-5 mt-5">
+					<div className="col">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
 					</div>
-
-					<div>
-						<h1>Section 2</h1>
+					
+					<div className="col me-5">
+						<h2 className="text-white">Title</h2>
+						<h5 className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque sed mauris eget posuere. Quisque malesuada nibh ac massa imperdiet, porttitor vulputate justo volutpat. Etiam dolor risus, interdum a massa sed, faucibus finibus sem. Nulla molestie molestie quam, a consequat neque molestie sit amet. Vivamus efficitur faucibus risus quis convallis. Morbi id ex volutpat, pretium nulla at, luctus justo. Proin condimentum finibus lorem et tristique.</h5>
 					</div>
 				</div>
 			</div>
 
-			<div className="ocean">
-			  <div className="wave"></div>
-			  <div className="wave"></div>
+			<div className = "par1"></div>
+
+			<div className="container">
+				<div className="d-flex flex-row-reverse mb-5 mt-5">
+					<div className="col">
+						<img className="card-img" src="https://franticallyspeaking.com/wp-content/uploads/2021/06/ART-8-PIC.jpg" />	
+					</div>
+					
+					<div className="col me-5">
+						<h2 className="text-white">Title</h2>
+						<h5 className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque sed mauris eget posuere. Quisque malesuada nibh ac massa imperdiet, porttitor vulputate justo volutpat. Etiam dolor risus, interdum a massa sed, faucibus finibus sem. Nulla molestie molestie quam, a consequat neque molestie sit amet. Vivamus efficitur faucibus risus quis convallis. Morbi id ex volutpat, pretium nulla at, luctus justo. Proin condimentum finibus lorem et tristique.</h5>
+					</div>
+				</div>
 			</div>
+
+
+			
 		</>
 	);
 }
